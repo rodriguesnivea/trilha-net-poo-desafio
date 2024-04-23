3c7375020712a0c12f5c8fbb85d251272bf457a1
rodriguesnivea <rodriguesnivea779@gmail.com>
2024-04-23 09:49:01 -0300

    Adição de instâncias de Smartphone Nokia e iPhone com instalação de aplicativos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+
+Smartphone nokia = new Nokia(numero: "(27) 98693-4219", modelo: "C01 Plus", imei: "35820277", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "(81) 99974-2173", modelo: "iPhone 15 Pro Max", imei: "67994426", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Linkedin");
